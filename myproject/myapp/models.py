cd18724d88848df607e2cfda3c590ebe924c2815
Ethan Bhatt <ethanbhatt2010@gmail.com>
2025-04-30 23:42:33 -0400

    some changes to frontend and database fields

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -8,8 +8,9 @@ class Application(models.Model):
     date = models.DateField(auto_now=True)
     positionTitle = models.CharField(max_length=100)
     companyName = models.CharField(max_length=100)
-    jobCategory = models.CharField(max_length=100)
+    jobCategory = models.CharField(max_length=50)
     coverLetter = models.CharField(max_length=5)
+    discoveryMethod = models.CharField(max_length=15)
 
     def __str__(self):
         return self.name
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -12,13 +12,18 @@ def submit(request):
         companyName = request.POST.get('companyName')
         jobCategory = request.POST.get('jobCategory')
         coverLetter = request.POST.get('coverLetter')
+        discoveryMethod = request.POST.get('discoveryMethod')
+
+        if (positionTitle is "" or companyName is "" or jobCategory is ""
+            or coverLetter is "" or discoveryMethod is ""):
+            return render(request, 'error.html')
 
         # Insert Data into PostgreSQL
         a = Application(positionTitle=positionTitle, companyName=companyName,
-                        jobCategory=jobCategory, coverLetter=coverLetter)
+                        jobCategory=jobCategory, coverLetter=coverLetter, discoveryMethod=discoveryMethod)
         a.save()
         
         # Now you can use this data, for example, validate or store it
-        return HttpResponse(f"Received application data!")
+        return render(request, 'success.html')
     else:
         return render(request, 'hello.html')
